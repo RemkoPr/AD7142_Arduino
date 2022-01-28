79102ab077ceb2a41df3e886ce0d9e10b906dde5
rproesma <Remko.Proesmans@UGent.be>
2022-01-28 17:16:46 +0100

    Initial commit

---
+++ ad7142.cpp
@@ -0,0 +1,326 @@
+#include "Arduino.h"
+#include "stdint.h"
+#include "ad7142.h"
+#include "settings.h"
+#include "Wire.h"
+
+
+AD7142::AD7142( uint8_t add0, uint8_t add1 ) {
+  // add0 is the least significant I2C address bit, add1 the second-to-least significant
+  if (add0 > 1 or add1 > 1) {
+    AD7142_LOG_ERROR("Invalid lower bits for I2C address!");
+  } else {
+    _address = AD7142_I2C_BASE_ADDRESS + (add1<<1) + add0;
+  }
+  _resultsRaw = new uint16_t[12]; // raw results register contents
+  _resultsPf = new float[12]; // results in pF
+}
+AD7142::~AD7142() {
+  delete[] _resultsRaw;
+}
+bool AD7142::init() {
+  Wire.begin(); 
+  if ( checkDeviceId() ) {
+    bool ret = true;
+    // Settings in comment are already true by default, included here to indicate the default settings.
+    // Arguments are declared in settings.h and start with an underscore.
+    ret &= setPwrControl(_pwrCtrl);
+    ret &= setInterrupt(_interruptCtrl);
+    ret &= setConnectionSetup(_setupCin0_6, _setupCin7_13);
+    ret &= enableAfeOffsets(_afeOffsetDisable);
+    ret &= setAfeOffsets(_afeOffset);
+    return ret;
+  } else {
+    AD7142_LOG_ERROR(("AD7142 Init failed: device ID/revision code did not match."));
+    return false;      
+  }
+}
+
+bool AD7142::checkDeviceId(void) {
+  AD7142_LOG_INFO(String("checkDeviceId: ") + String(readRegister(AD7142_REVISION_CODE_DEVID_ADDR)));
+  return (AD7142_REVISION_CODE_DEVID == readRegister(AD7142_REVISION_CODE_DEVID_ADDR));
+}
+
+// ******************
+//   Power control
+// ******************
+
+bool AD7142::setPwrControl( uint8_t pwrCtrl[8] ) {
+  // writes the entire register, fnctns below write specific bits
+  uint16_t val = 
+    pwrCtrl[0] +          //operatingMode
+    (pwrCtrl[1]<<2) +     //lowPowerConvDelay
+    (pwrCtrl[2]<<4) +     //sequenceStageNum
+    (pwrCtrl[3]<<8) +     //decimation
+    (pwrCtrl[4]<<11) +    //interruptPolarityControl
+    (pwrCtrl[5]<<12) +    //srcControl
+    (pwrCtrl[6]<<13) +    //invSrcControl
+    (pwrCtrl[7]<<14) ;    //cdcBiasCurrentControl
+  _numStages = pwrCtrl[2];
+  writeRegister(AD7142_PWR_CONTROL, val);
+}
+
+bool AD7142::setOperatingMode(uint8_t mode) {
+  if ( mode > 3 ) {
+    AD7142_LOG_ERROR("Invalid operating mode passed.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1111111111111100) | mode;
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setLowPowerConvDelay(uint8_t delay_val) {
+  if ( delay_val > 3 ) {
+    AD7142_LOG_ERROR("Invalid delay option passed.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1111111111110011) | (delay_val<<2);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setSequenceStageNum(uint8_t num) {
+  if ( num > 11 ) {
+    AD7142_LOG_ERROR("Invalid number of conversion stages passed.");
+    return false;
+  }
+  _numStages = num;
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1111111100001111) | (num<<4);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setDecimation(uint8_t rate) {
+  if ( rate > 3 ) {
+    AD7142_LOG_ERROR("Invalid decimation rate passed.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1111110011111111) | (rate<<8);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::swReset(void) {
+  return writeRegister(AD7142_PWR_CONTROL, 0x400); // 0000 0100 0000 0000
+}
+
+bool AD7142::setInterruptPolarityControl(uint8_t ctrl) {
+  if ( ctrl > 1 ) {
+    AD7142_LOG_ERROR("Invalid decimation polarity control passed.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1111011111111111) | (ctrl<<11);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setSrcControl(uint8_t ctrl) {
+  if ( ctrl > 1 ) {
+    AD7142_LOG_ERROR("Invalid source control passed for pin 15.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1110111111111111) | (ctrl<<12);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setInvSrcControl(uint8_t ctrl) {
+  if ( ctrl > 1 ) {
+    AD7142_LOG_ERROR("Invalid (inverted) source control passed for pin 16.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b1101111111111111) | (ctrl<<13);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+bool AD7142::setCdcBiasCurrentControl(uint8_t ctrl) {
+  if ( ctrl > 3 ) {
+    AD7142_LOG_ERROR("Invalid CDC bias current option passed.");
+    return false;
+  }
+  uint16_t pwr_control_reg = readRegister(AD7142_PWR_CONTROL);
+  pwr_control_reg = (pwr_control_reg & 0b0011111111111111) | (ctrl<<14);
+  return writeRegister(AD7142_PWR_CONTROL, pwr_control_reg);
+}
+
+// ************************
+//   Interrupt control
+// ************************
+
+bool AD7142::setInterrupt(uint8_t interruptCtrl[12][3]) {
+  uint16_t lowIntReg = 0;
+  uint16_t highIntReg = 0;
+  uint16_t completeIntReg = 0;
+  for( int stage = 0 ; stage < 3 ; stage++ ) {
+    lowIntReg       += interruptCtrl[stage][0] << stage;
+    highIntReg      += interruptCtrl[stage][1] << stage;
+    completeIntReg  += interruptCtrl[stage][2] << stage;
+  }
+  bool ret = true;
+  ret &= writeRegister(AD7142_STAGE_LOW_INT_EN, lowIntReg);
+  ret &= writeRegister(AD7142_STAGE_HIGH_INT_EN, highIntReg);
+  ret &= writeRegister(AD7142_STAGE_COMPLETE_INT_EN, completeIntReg);
+  return ret;
+}
+
+// ************************
+//   CIN connection setup
+// ************************
+
+bool AD7142::setConnectionSetup(uint8_t cin0_6[12][7], uint8_t cin7_13[12][7]) {
+  bool ret = true;
+  for (int stage = 0 ; stage < 12 ; stage++ ) {
+    uint16_t regContent0_6 = 0x0000;
+    uint16_t regContent7_13 = 0x0000;
+    for (int x = 0 ; x < 7 ; x++ ) {
+      regContent0_6 = regContent0_6 | (cin0_6[stage][x] << 2*x);
+      regContent7_13 = regContent7_13 | (cin7_13[stage][x] << 2*x);
+    }
+    ret &= writeRegister(AD7142_STAGE0_CONNECTION_CIN6_0 + stage*AD7142_STAGEX_BANK2_ADDR_OFFSET, regContent0_6);
+    ret &= writeRegister(AD7142_STAGE0_CONNECTION_CIN13_7 + stage*AD7142_STAGEX_BANK2_ADDR_OFFSET, regContent7_13);
+  }
+  return ret;
+}
+
+bool AD7142::enableAfeOffsets(uint8_t afeOffsetDisable[12][2]) {
+  bool ret = true;
+  for( int stage = 0 ; stage < 12 ; stage++ ) {
+    if( afeOffsetDisable[stage][0] > 1 or afeOffsetDisable[stage][1] > 1 ) {
+      AD7142_LOG_ERROR(String("Invalid value passed to enableAfeOffsets for stage ") + String(stage));
+    }
+    uint8_t val = afeOffsetDisable[stage][0] + (afeOffsetDisable[stage][1]<<1);
+    ret &= writeRegisterBits(
+        AD7142_STAGE0_CONNECTION_CIN13_7 + stage*AD7142_STAGEX_BANK2_ADDR_OFFSET, 
+        14, 15, val
+        );
+  }
+  return ret;
+}
+
+/*uint8_t pfTo7bit(uint8_t pf) {
+  // helper function that translates pF to a 7bit offset value for offset control
+  if( pf > 20 ) {
+    AD7142_LOG_ERROR("Invalid offset [pF] passed.");
+  }
+  // AD7142_LOG_DEBUG(String("pfTo7bit [pf] [output]: ") + String(pf) + String((uint8_t)round(pf / 0.16)));
+  return (uint8_t)round(pf / 0.16);
+}*/
+
+bool AD7142::setAfeOffsets(uint8_t offsets[12][4]) {
+  bool ret = true;
+  for( int stage = 0 ; stage < 12 ; stage++ ) {
+    uint16_t val = 
+      (offsets[stage][0]) + 
+      (offsets[stage][1]<<7) + 
+      (offsets[stage][2]<<8) + 
+      (offsets[stage][3]<<15);
+    ret &= writeRegister(
+      AD7142_STAGE0_AFE_OFFSET + stage*AD7142_STAGEX_BANK2_ADDR_OFFSET,
+      val
+      );
+  }
+  return ret;
+}
+
+// *****************************
+//    Actual functionality
+// *****************************
+
+void AD7142::readResults() {
+  for( int stage = 0 ; stage < _numStages ; stage++ ) {
+    _resultsRaw[stage] = readRegister(AD7142_STAGE0_CONV_DATA + stage*AD7142_STAGEX_BANK3_ADDR_OFFSET);
+  }
+  formatResults();
+}
+
+void AD7142::formatResults() {
+  for( int stage = 0 ; stage < _numStages ; stage++ ) {
+    uint16_t rawVal = _resultsRaw[stage];
+    float pfVal = rawVal / 2450.0 * 0.16;
+    _resultsPf[stage] = pfVal;
+  }
+}
+
+void AD7142::printResults() {
+  for( int stage = 0 ; stage < _numStages ; stage++ ) {
+    AD7142_LOG_ANY(String("Stage ") + String(stage) + String(": ") + String(_resultsRaw[stage]) + String(" / ") + String(_resultsPf[stage]) + String("pF"));
+  }
+}
+
+// *****************************
+//    Low-level register I/O
+// *****************************
+
+bool AD7142::writeRegister(const uint16_t register_addr, const uint16_t val) {
+  AD7142_LOG_DEBUG(String("Attempting write of value ") + String(val) + String(" to register ") + String(register_addr));
+  Wire.beginTransmission(_address);
+  Wire.write(highByte(register_addr));  
+  Wire.write(lowByte(register_addr));
+  Wire.write(highByte(val));  
+  Wire.write(lowByte(val));
+  byte result = Wire.endTransmission(true);
+  return checkTransmissionResult(result);
+}
+
+bool AD7142::writeRegisterBits(const uint16_t register_addr, uint16_t start_bit, uint16_t end_bit, const uint16_t val) {
+  /*
+   * Writes specific conescutive bits of a register. The LSB is bit 0.
+   * If 1 bit is to be written, start_bit and end_bit are
+   * the same, i.e. end_bit is included in written bits.
+   */
+  if ( start_bit > 15 or end_bit > 15 ) {
+    AD7142_LOG_ERROR("Invalid bit range passed to writeRegisterBits.");
+    return false;
+  } else if ( val >= pow(2, end_bit - start_bit + 1) ) {
+    AD7142_LOG_ERROR("Value too large for passed bit range.");
+    return false;
+  }
+  uint16_t reg_content = readRegister(register_addr);
+  uint16_t mask = pow(2, 16) - 1;
+  for (int i = start_bit ; i <= end_bit ; i++ ) {
+    mask = mask - (uint16_t)pow(2, i);
+  }
+  reg_content = (reg_content & mask) | (val<<start_bit);
+  return writeRegister(register_addr, reg_content);
+}
+
+uint16_t AD7142::readRegister(const uint16_t register_addr) {
+  AD7142_LOG_DEBUG(String("Attempting read from register ") + String(register_addr));
+  
+  Wire.beginTransmission(_address); //open communication with 
+  Wire.write(highByte(register_addr));  
+  Wire.write(lowByte(register_addr));
+  byte result = Wire.endTransmission(); 
+  checkTransmissionResult(result);
+
+  Wire.requestFrom(_address, 2); // 16-bit registers
+  uint16_t buf;
+  Wire.readBytes((uint8_t*)&buf, 2);
+  buf = (buf << 8) | (buf >> 8); // Flip bytes
+  return buf;
+}
+
+bool AD7142::checkTransmissionResult(byte result) {
+  switch ( result ) {
+    case 0:
+      AD7142_LOG_DEBUG("Succesfully wrote to register.");
+      return(true);
+    case 1:
+      AD7142_LOG_ERROR("Data too long to fit in transmit buffer.");
+      return(false);
+    case 2:
+      AD7142_LOG_ERROR("Received NACK on transmit of address.");
+      return(false);
+    case 3:
+      AD7142_LOG_ERROR("Received NACK on transmit of data.");
+      return(false);
+    case 4:
+      AD7142_LOG_ERROR("Unknown error.");
+      return(false);
+    default:
+      AD7142_LOG_ERROR("Unknown return value of Wire.endTransmission, check documentation.");
+      return(false);
+  }
+}
---
+++ ad7142.h
@@ -0,0 +1,319 @@
+#ifndef _AD7142_H_
+#define _AD7142_H_
+
+
+#define AD7142_I2C_BASE_ADDRESS           0x02C // 0101 100
+
+// set the profile (AD7142_PROFILE) to determine which settings are used form settings.h
+#define AD7142_PROFILE_5X5                0
+#define AD7142_PROFILE_SINGLE             1
+#define AD7142_PROFILE_DEBUG              2
+#define AD7142_PROFILE                    AD7142_PROFILE_DEBUG 
+
+// ***********
+//  Registers
+// ***********
+
+// Bank 1
+#define AD7142_PWR_CONTROL                0x000
+#define AD7142_STAGE_CAL_EN               0x001
+#define AD7142_AMB_COMP_CTRL0             0x002
+#define AD7142_AMB_COMP_CTRL1             0x003
+#define AD7142_AMB_COMP_CTRL2             0x004
+#define AD7142_STAGE_LOW_INT_EN           0x005
+#define AD7142_STAGE_HIGH_INT_EN          0x006
+#define AD7142_STAGE_COMPLETE_INT_EN      0x007
+#define AD7142_STAGE_LOW_LIMIT_INT        0x008
+#define AD7142_STAGE_HIGH_LIMIT_INT       0x009
+#define AD7142_STAGE_COMPLETE_LIMIT_INT   0x00A
+#define AD7142_ADC_RESULT_S0              0x00B
+#define AD7142_ADC_RESULT_S1              0x00C
+#define AD7142_ADC_RESULT_S2              0x00D
+#define AD7142_ADC_RESULT_S3              0x00E
+#define AD7142_ADC_RESULT_S4              0x00F
+#define AD7142_ADC_RESULT_S5              0x010
+#define AD7142_ADC_RESULT_S6              0x011
+#define AD7142_ADC_RESULT_S7              0x012
+#define AD7142_ADC_RESULT_S8              0x013
+#define AD7142_ADC_RESULT_S9              0x014
+#define AD7142_ADC_RESULT_S10             0x015
+#define AD7142_ADC_RESULT_S11             0x016
+#define AD7142_REVISION_CODE_DEVID_ADDR   0x017 // default value: 0xE622 = 1110 0110 0010 0010
+#define AD7142_STAGE_PROXIMITY_STATUS     0x042
+
+// Bank 2: CIN connection setup per STAGE
+/* 
+ *  To access the STAGEX connection setup registers,
+ *   add X * AD7142_STAGEX_BANK2_ADDR_OFFSET to the respective
+ *   STAGE0 register address
+*/
+#define AD7142_STAGE0_CONNECTION_CIN6_0   0x080
+#define AD7142_STAGE0_CONNECTION_CIN13_7  0x081
+#define AD7142_STAGE0_AFE_OFFSET          0x082
+#define AD7142_STAGE0_SENSITIVITY         0x083
+#define AD7142_STAGE0_OFFSET_LOW          0x084
+#define AD7142_STAGE0_OFFSET_HIGH         0x085
+#define AD7142_STAGE0_OFFSET_HIGH_CLAMP   0x086
+#define AD7142_STAGE0_OFFSET_LOW_CLAMP    0x087
+#define AD7142_STAGEX_BANK2_ADDR_OFFSET   0x008 
+
+// Bank 3
+/* 
+ *  To access the STAGEX results registers,
+ *   add X * AD7142_STAGEX_BANK3_ADDR_OFFSET to the respective
+ *   STAGE0 register address
+*/
+#define AD7142_STAGE0_CONV_DATA           0x0E0
+#define AD7142_STAGE0_FF_WORD0            0x0E1
+#define AD7142_STAGE0_FF_WORD1            0x0E2
+#define AD7142_STAGE0_FF_WORD2            0x0E3
+#define AD7142_STAGE0_FF_WORD3            0x0E4
+#define AD7142_STAGE0_FF_WORD4            0x0E5
+#define AD7142_STAGE0_FF_WORD5            0x0E6
+#define AD7142_STAGE0_FF_WORD6            0x0E7
+#define AD7142_STAGE0_FF_WORD7            0x0E8
+#define AD7142_STAGE0_SF_WORD0            0x0E9
+#define AD7142_STAGE0_SF_WORD1            0x0EA
+#define AD7142_STAGE0_SF_WORD2            0x0EB
+#define AD7142_STAGE0_SF_WORD3            0x0EC
+#define AD7142_STAGE0_SF_WORD4            0x0ED
+#define AD7142_STAGE0_SF_WORD5            0x0EE
+#define AD7142_STAGE0_SF_WORD6            0x0EF
+#define AD7142_STAGE0_SF_WORD7            0x0F0
+#define AD7142_STAGE0_SF_AMBIENT          0x0F1
+#define AD7142_STAGE0_FF_AVG              0x0F2
+#define AD7142_STAGE0_PEAK_DETECT_WORD0   0x0F3
+#define AD7142_STAGE0_PEAK_DETECT_WORD1   0x0F4
+#define AD7142_STAGE0_MAX_WORD0           0x0F5
+#define AD7142_STAGE0_MAX_WORD1           0x0F6
+#define AD7142_STAGE0_MAX_WORD2           0x0F7
+#define AD7142_STAGE0_MAX_WORD3           0x0F8
+#define AD7142_STAGE0_MAX_AVG             0x0F9
+#define AD7142_STAGE0_HIGH_THRESHOLD      0x0FA
+#define AD7142_STAGE0_MAX_TEMP            0x0FB
+#define AD7142_STAGE0_MIN_WORD0           0x0FC
+#define AD7142_STAGE0_MIN_WORD1           0x0FD
+#define AD7142_STAGE0_MIN_WORD2           0x0FE
+#define AD7142_STAGE0_MIN_WORD3           0x0FF
+#define AD7142_STAGE0_MIN_AVG             0x100
+#define AD7142_STAGE0_LOW_THRESHOLD       0x101
+#define AD7142_STAGE0_MIN_TEMP            0x102
+// 0x103 unused
+#define AD7142_STAGEX_BANK3_ADDR_OFFSET   0x024 
+
+// ***********
+//  Settings
+//   Bank 1
+// ***********
+
+// PWR_CONTROL bit options, datasheet Table 20
+#define AD7142_POWER_MODE_FULL            0x00
+#define AD7142_POWER_MODE_LOW             0x02
+#define AD7142_POWER_MODE_SHTDN           0x01
+#define AD7142_POWER_MODE_SHTDN_          0x03
+
+#define AD7142_LP_CONV_DELAY_200          0x00 // 200ms
+#define AD7142_LP_CONV_DELAY_400          0x01 // 400ms
+#define AD7142_LP_CONV_DELAY_600          0x02 // 600ms
+#define AD7142_LP_CONV_DELAY_800          0x03 // 800ms
+
+#define AD7142_DECIMATION_256             0x00
+#define AD7142_DECIMATION_128             0x01
+#define AD7142_DECIMATION_NONE            0x02
+#define AD7142_DECIMATION_NONE_           0x03
+
+#define AD7142_INT_POL_ACTIVE_LOW         0x00
+#define AD7142_INT_POL_ACTIVE_HIGH        0x01
+
+#define AD7142_EXCITATION_SOURCE_ENABLE   0x00 // pin 15
+#define AD7142_EXCITATION_SOURCE_DISABLE  0x01
+
+#define AD7142_SRC_ENABLE                 0x00 // pin 16
+#define AD7142_SRC_DISABLE                0x01
+
+#define AD7142_CDC_BIAS_NORMAL            0x00
+#define AD7142_CDC_BIAS_PLUS20            0x01
+#define AD7142_CDC_BIAS_PLUS35            0x02
+#define AD7142_CDC_BIAS_PLUS50            0x03
+
+// STAGE_CAL_EN bit options, datasheet Table 21
+#define AD7142_STAGEX_CAL_EN_DISABLE      0x00
+#define AD7142_STAGEX_CAL_EN_ENABLE       0x01
+
+#define AD7142_AVG_FP_SKIP_3              0x00
+#define AD7142_AVG_FP_SKIP_7              0x01
+#define AD7142_AVG_FP_SKIP_15             0x02
+#define AD7142_AVG_FP_SKIP_31             0x03
+
+#define AD7142_AVG_LP_SKIP_NONE           0x00
+#define AD7142_AVG_LP_SKIP_1              0x01
+#define AD7142_AVG_LP_SKIP_2              0x10
+#define AD7142_AVG_LP_SKIP_3              0x11
+
+// AMB_COMP_CTRL0 bit options, datasheet Table 22
+  // TODO
+// AMB_COMP_CTRL1 bit options, datasheet Table 23
+  // TODO
+// AMB_COMP_CTRL2 bit options, datasheet Table 24
+  // TODO
+
+// STAGE_LOW_INT_EN bit options, datasheet Table 25
+#define AD7142_STAGEX_LOW_INT_DISABLE     0x00
+#define AD7142_STAGEX_LOW_INT_ENABLE      0x01 //INT asserted if STAGEX low threshold is exceeded
+
+#define AD7142_GPIO_SETUP_DISABLE         0x00
+#define AD7142_GPIO_SETUP_INPUT           0x01
+#define AD7142_GPIO_SETUP_OUTPUT_ACTIVE_LOW     0x02
+#define AD7142_GPIO_SETUP_OUTPUT_ACTIVE_HIGH    0x03
+
+#define AD7142_GPIO_INPUT_CONFIG_TRIG_NEG_LVL   0x00
+#define AD7142_GPIO_INPUT_CONFIG_TRIG_POS_EDGE  0x01
+#define AD7142_GPIO_INPUT_CONFIG_TRIG_NEG_EDGE  0x02
+#define AD7142_GPIO_INPUT_CONFIG_TRIG_POS_LVL   0x03
+
+// STAGE_HIGH_INT_EN bit options, datasheet Table 26
+#define AD7142_STAGEX_HIGH_INT_DISABLE    0x00
+#define AD7142_STAGEX_HIGH_INT_ENABLE     0x01 // INT asserted if STAGEX high threshold is exceeded
+
+// STAGE_COMPLETE_INT_EN bit options, datasheet Table 27
+#define AD7142_STAGEX_COMPLETE_DISABLE    0x00
+#define AD7142_STAGEX_COMPLETE_ENABLE     0x01 // INT asserted at completion of STAGE0 conversion
+
+#define AD7142_GPIO_INT_ENABLE            0x00
+#define AD7142_GPIO_INT_DISABLE           0x01
+
+// STAGE_HIGH_LIMIT_INT bit options, datasheet Table 28
+#define AD7142_STAGEX_LOW_LIMIT_FALSE     0x00
+#define AD7142_STAGEX_LOW_LIMIT_TRUE      0x01
+
+// STAGE_HIGH_LIMIT_INT bit options, datasheet Table 29
+#define AD7142_STAGEX_HIGH_LIMIT_FALSE     0x00
+#define AD7142_STAGEX_HIGH_LIMIT_TRUE      0x01
+
+// STAGE_COMPLETE_LIMIT_INT bit options, datasheet Table 30
+#define AD7142_STAGEX_COMPLETE_FALSE       0x00
+#define AD7142_STAGEX_COMPLETE_TRUE        0x01
+
+// CDC 16-Bit Conversion Data Registers, datasheet Table 31
+// Use corresponding registers in Bank3 instead
+
+// datasheet Table 32
+#define AD7142_REVISION_CODE_DEVID        0xE622
+
+// Proximity Status Register, datasheet Table 33
+#define AD7142_STAGEX_PROXIMITY_FALSE     0x00
+#define AD7142_STAGEX_PROXIMITY_TRUE      0x01
+
+// ***********
+//  Settings
+//   Bank 2
+// ***********
+
+// CIN connection setup, datasheet Table 46, 47, 48
+#define AD7142_CIN_NOT_CONNECTED          0x00
+#define AD7142_CIN_CONNECTED_NEG          0x01
+#define AD7142_CIN_CONNECTED_POS          0x02
+#define AD7142_CIN_CONNECTED_BIAS         0x03
+
+#define AD7142_AFE_OFFSET_ENABLE          0x00
+#define AD7142_AFE_OFFSET_DISABLE         0x01
+
+#define AD7142_AFE_OFFSET_SWAP_FALSE      0x00
+#define AD7142_AFE_OFFSET_SWAP_TRUE       0x01
+
+// TODO table 49 sensitivity settings
+
+// ***********
+//  Settings
+//   Bank 3
+// ***********
+
+  // TODO
+
+class AD7142 {
+ public:
+
+    AD7142(uint8_t add0, uint8_t add1); 
+    ~AD7142(); 
+
+    bool init(void);                                    
+    bool checkDeviceId(void);
+
+    // Power control
+    bool setPwrControl( uint8_t pwrCtrl[8] ); // writes the entire register, fnctns below write specific bits
+    bool setOperatingMode(uint8_t mode);
+    bool setLowPowerConvDelay(uint8_t delay_val);
+    bool setSequenceStageNum(uint8_t num);
+    bool setDecimation(uint8_t rate);
+    bool swReset(void);
+    bool setInterruptPolarityControl(uint8_t ctrl);
+    bool setSrcControl(uint8_t ctrl);
+    bool setInvSrcControl(uint8_t ctrl);
+    bool setCdcBiasCurrentControl(uint8_t ctrl);
+
+    // Interrupt control
+    bool setInterrupt(uint8_t interruptCtrl[12][3]);
+    
+    // CIN connection setup
+    bool setConnectionSetup(uint8_t cin0_6[12][7], uint8_t cin7_13[12][7]);
+    bool enableAfeOffsets(uint8_t afeOffsetDisable[12][2]);
+    bool setAfeOffsets(uint8_t offsets[12][4]);
+
+    // Getters & setters
+    uint16_t* getResultsRaw() { return _resultsRaw; } 
+    float* getResultsPf() { return _resultsPf; } 
+    uint8_t getNumStages() { return _numStages; } 
+
+    // Actual functionality
+    void readResults();
+    void formatResults();
+    void printResults();
+
+    uint16_t readRegister(const uint16_t register_addr);
+ private:
+    uint8_t _address;  // I2C address
+    uint16_t* _resultsRaw;
+    float* _resultsPf;
+    uint8_t _numStages; // number of conversion stages
+
+    bool writeRegister(const uint16_t register_addr, const uint16_t value);
+    bool writeRegisterBits(const uint16_t register_addr, uint16_t start_bit, uint16_t end_bit, const uint16_t value);
+    
+    bool checkTransmissionResult(byte result);
+};
+
+
+// Logger wrapper
+#define AD7142_LOG_LEVEL 5
+
+#if AD7142_LOG_LEVEL >= 5
+#define AD7142_LOG_DEBUG(x) Serial.println(String("DEBUG: ") + String(x))
+#else
+#define AD7142_LOG_DEBUG(x) // nothing
+#endif
+
+#if AD7142_LOG_LEVEL >= 4
+#define AD7142_LOG_INFO(x) Serial.println(String("INFO: ") + String(x))
+#else
+#define AD7142_LOG_INFO(x) // nothing
+#endif
+
+#if AD7142_LOG_LEVEL >= 3
+#define AD7142_LOG_WARN(x) Serial.println(String("WARNING: ") + String(x))
+#else
+#define AD7142_LOG_WARN(x) // nothing
+#endif
+
+#if AD7142_LOG_LEVEL >= 2
+#define AD7142_LOG_ERROR(x) Serial.println(String("ERROR: ") + String(x))
+#else
+#define AD7142_LOG_ERROR(x) // nothing
+#endif
+
+#if AD7142_LOG_LEVEL >= 1
+#define AD7142_LOG_ANY(x) Serial.println(x)
+#else
+#define AD7142_LOG_ANY(x) // nothing
+#endif
+
+#endif /* _AD7142_H_ */
